afecd801c3938b795bc1132fce38d75355a92f76
sabarirajpazhani <sabarirajpazhani@gmail.com>
2025-04-24 23:01:40 +0530

    Add project files.

---
+++ C_Sharps_Arrays/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
+namespace C_Sharps_Arrays
+{
+    public class Program
+    {
+        public static void CreatingList()
+        {
+            List<int> l = new List<int>();
+            l.Add(10);
+            l.Add(20);
+            l.Add(30);
+            foreach(int i in l)
+            {
+                Console.WriteLine(i);
+            }
+
+            List<object> obj = new List<object>();
+            obj.Add("Sabari");
+            obj.Add(1);
+            obj.Add('c');
+            obj.Add(null);
+            foreach(var i in obj)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        public static void addRange()
+        {
+            int[] arr = new int[3] { 1, 2, 4 };
+            List<int> l = new List<int>();
+            l.Add(5);
+            l.Add(4);
+            l.AddRange(arr);
+
+            l.ForEach(x => Console.WriteLine(x));
+        }
+        
+        public static void retriving()
+        {
+            List<int> l = new List<int>() { 1, 2, 3, 4 };
+
+            Console.WriteLine(l[3]);
+        }
+        static void Main(String [] args)
+        {
+            CreatingList();
+            addRange();
+            retriving();
+
+        }
+    }
+}
